78982151b95b71155021a5507330d5ac324c16a6
takudzwajoseph <takudzwajosep19@gmail.com>
2023-03-27 11:34:19 +0200

    add changes

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.omload = function () {
+    qindow.addEventListener('scroll', function (e) {
+        if (window.pageYOffset > 100){
+            document.querySelector("header").classList.add('is-scrolling');
+        } else{
+            document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
+
+    const menu_btn = document.querySelector('.hamburger');
+
+    menu_btn.addEventListener('click', function (){
+        menu_btn.classList.toggle('is-active');
+    });
+}
