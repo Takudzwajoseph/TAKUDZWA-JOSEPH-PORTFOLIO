0ba636d97d8b53b65cfc424b870e5b536adc787b
dylanmaupa <114060288+dylanmaupa@users.noreply.github.com>
2023-09-18 16:23:50 +0200

    #

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-window.omload = function () {
-    qindow.addEventListener('scroll', function (e) {
-        if (window.pageYOffset > 100){
-            document.querySelector("header").classList.add('is-scrolling');
-        } else{
-            document.querySelector("header").classList.remove('is-scrolling');
-        }
-    });
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function () {
+  const nav = document.querySelector(".nav");
+  hamburger.classList.toggle("is-active");
+  nav.classList.toggle("active");
+};
 
-    const menu_btn = document.querySelector('.hamburger');
+window.onload = function () {
+  window.addEventListener("scroll", function (e) {
+    if (window.pageYOffset > 100) {
+      document.querySelector("header").classList.add("is-scrolling");
+    } else {
+      document.querySelector("header").classList.remove("is-scrolling");
+    }
+  });
 
-    menu_btn.addEventListener('click', function (){
-        menu_btn.classList.toggle('is-active');
-    });
-}
+  // const menu_btn = document.querySelector('.hamburger');
+
+  // menu_btn.addEventListener('click', function (){
+  //     menu_btn.classList.toggle('is-active');
+
+  //     if(menu_btn == 'is-active'){
+  //         nav.classList.add('active')
+  //     }
+  // });
+};
